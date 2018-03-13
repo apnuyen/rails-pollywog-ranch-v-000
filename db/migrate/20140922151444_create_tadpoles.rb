8c3c42c5cbf316d00330fc3c6776f3a735c14670
An Nuyen <annuyen@gmail.com>
2018-03-13 17:40:01 +0000

    automatically backed up by learn

--- db/migrate/20140922151444_create_tadpoles.rb
+++ db/migrate/20140922151444_create_tadpoles.rb
@@ -2,8 +2,8 @@ class CreateTadpoles < ActiveRecord::Migration
   # code goes here
   def change
     create_table :tadpoles do |t|
-      t.string :name 
-      t.string :color 
-    end 
-  end 
+      t.string :name
+      t.string :color
+    end
+  end
 end
